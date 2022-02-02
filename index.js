007725981229cf014c3785b04bb5287e554ceb03
CalvinPierce <calvinpierce99@gmail.com>
2022-02-02 18:37:07 -0500

    Inital lab changes done 6:37

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json()); // Make sure it comes back as json
 
 //TODO - Replace you Connection String here
-mongoose.connect('PASTE_YOUR_CONNECTION_STRING_HERE', {
+mongoose.connect('mongodb+srv://Calvin:XhNk6ITISKWUm1ir@comp3123.c34yz.mongodb.net/w2022_comp3133?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
--- models/Employee.js
+++ models/Employee.js
@@ -2,31 +2,64 @@ const mongoose = require('mongoose');
 
 const EmployeeSchema = new mongoose.Schema({
   firstname: {
-    type: String
+    type: String,
+    required: [true, "Please Enter First Name"],
+    trim: true,
+    lowercase: true
   },
   lastname: {
-    type: String
+    type: String,
+    alias: 'surname',
+    required: true,
+    trim: true,
+    lowercase: true
   },
   email: {
-    type: String
+    type: String,
+    required: true,
+    trim: true,
+    uppercase: true,
+    minlength: 5,
+    maxlength: 50,
+    validate: function(value) {
+      var emailRegex = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+      return emailRegex.test(value);
+    }
   },
   gender: {
-    type: String
+    type: String,
+    required: true,
+    enum:['male', 'female', 'other']
   },
   city:{
-    type: String
+    type: String,
+    required: true,
+    trim: true
   },
   designation: {
-    type: String
+    type: String,
+    required: true,
+    trim: true
   },
   salary: {
-    type: Number
+    type: Number,
+    default: 0.0,
+    //min: [1000, 'Salary must be greater than 1000'],
+    //max: 25000
+    validate: function(value){
+      if(value < 0){
+        throw new Error("Negative Salary not allowed!")
+      }
+    }
   },
   created: { 
-    type: Date
+    type: Date,
+    default: Date.now,
+    alias: 'createdat'
   },
   updatedat: { 
-    type: Date
+    type: Date,
+    default: Date.now
   },
 });
 
